b82dacf438838d1709992d64ae492496e28c58b0
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-08-25 19:16:39 +0700

    Lesson 4

---
+++ Lesson4.py
@@ -0,0 +1,25 @@
+# ===================== Luyện tập =====================
+# Câu 1: Nhập một số từ bàn phím và in ra số đó.
+
+# Câu 2: Viết chương trình kiểm tra nhập vào 1 số và kiểm tra số đó là chẵn hay lẻ.
+
+# Câu 3: Viết chương trình tính tổng, hiệu, tích, thương, chia lấy nguyên, chia lấy dư, lũy thừa của hai số nhập từ bàn phím.
+
+# Câu 4: Viết chương trình chuyển đổi từ USD sang VND (số tiền được nhập từ bàn phím).
+
+# Bài 5: Nhập số điện bạn sử dụng (kWh)
+# Tính tiền điện theo dữ liệu sau và hiển thị ra màn hình
+# Bậc 1:    0kWh - 50kWh           giá 1.8k VND / kWh
+# Bậc 2:    51kWh - 100kWh         giá 2k VND / kWh
+# Bậc 3:    101kWh - 200kWh        giá 2.3k VND / kWh
+# Bậc 4:    trên 201kWh            giá 3k VND / kWh
+
+# Bài 6: Nhập số giây
+# yêu cầu: Chuyển sang định dạng giờ phút giây
+# VD: 3661s = 1h 1m 1s
+
+# Bài 7: Chia m cái kẹo cho n học sinh
+# Yêu cầu:
+    # Nhập m, n từ bàn phím
+    # Tính số kẹo mỗi học sinh được nhận
+    # Tính số kẹo còn lại sau khi chia
