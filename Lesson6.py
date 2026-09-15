4eec63b53a3dc7bb8cb2c8bc4748c3778cf1f8c6
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-09-15 19:27:39 +0700

    Lesson 6

---
+++ Lesson6.py
@@ -0,0 +1,34 @@
+# Vòng lặp hữu hạn - Vòng lặp for
+
+# Cú pháp đầy đủ range(start, stop, step)
+    # start: giá trị bắt đầu (không bắt buộc, mặc định = 0)
+    # stop: giá trị kết thúc (bắt buộc)
+    # step: bước nhảy (không bắt buộc, mặc định = 1)
+# Lưu ý: chạy từ start đến stop-1
+
+# TH1: range(start, stop, step)
+# TH2: range(start, stop)
+# TH3: range(stop)
+
+# Ví dụ:
+# range(5,10): 5 6 7 8 9
+# range(2, 10, 2): 2 4 6 8
+# range(-5): không chạy
+# range(-10, -5): -10 -9 -8 -7 -6
+# range(1): 0
+# range(2,8,3): 2 5
+# range(2): 0 1
+# range(-5, 2): -5 -4 -3 -2 -1 0 1
+
+# ===================== LUYỆN TẬP =====================
+# Bài 1: Nhập 2 số nguyên a và b từ bàn phím.
+# In ra các số nguyên trong khoảng [a, b]
+
+# Bài 2: Nhập 2 số nguyên a và b từ bàn phím.
+# In ra các số nguyên trong khoảng [a, b] nếu b >= a
+# In ra các số nguyên trong khoảng [b, a] nếu a > b
+
+# Bài 3: Nhập 1 số nguyên a trong khoảng [1, 10]
+# In ra màn hình bảng cửu chương a
+
+# Bài 4: In ra màn hình bảng cửu chương từ 2 => 9
