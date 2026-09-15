6b0ebd4bb3c1cb281a2b2d7089a01c6ab9b25712
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-09-15 20:36:09 +0700

    Lesson 6

--- Lesson6.py
+++ Lesson6.py
@@ -23,12 +23,37 @@
 # ===================== LUYỆN TẬP =====================
 # Bài 1: Nhập 2 số nguyên a và b từ bàn phím.
 # In ra các số nguyên trong khoảng [a, b]
+a = int(input("Nhập số nguyên a: "))
+b = int(input("Nhập số nguyên b: "))
+
+for i in range(a, b + 1):
+    print(i, end=" ")
 
 # Bài 2: Nhập 2 số nguyên a và b từ bàn phím.
 # In ra các số nguyên trong khoảng [a, b] nếu b >= a
 # In ra các số nguyên trong khoảng [b, a] nếu a > b
+a = int(input("\nNhập số nguyên a: "))
+b = int(input("Nhập số nguyên b: "))
+
+if b >= a:
+    for i in range(a, b + 1):
+        print(i, end=" ")
+else:
+    for i in range(b, a + 1):
+            print(i, end=" ")
 
 # Bài 3: Nhập 1 số nguyên a trong khoảng [1, 10]
 # In ra màn hình bảng cửu chương a
+a = int(input('\nNhập a trong khoảng [1,10]: '))
+
+if 1 <= a <= 10:
+    for i in range(1, 11):
+        print(f'{a} x {i} = {a*i}')
+else:
+    print("Nhập sai a, a phải nằm trong khoảng [1,10]")
 
 # Bài 4: In ra màn hình bảng cửu chương từ 2 => 9
+for a in range(2, 10):
+    print("\nBảng cửu chương", a)
+    for i in range(1, 11):
+        print(f'{a} x {i} = {a*i}')
